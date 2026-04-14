7c974fe6d3bd01a04d53ab05b59547e9807ea0a9
BARRAVELHA <matheusnplatt@gmail.com>
2026-04-14 17:38:40 -0300

    Add fuel consumption calculation to carro.js

---
+++ carro.js
@@ -0,0 +1,6 @@
+let distancia = 300;
+        let combustivel = 25;
+        
+        let consumo_medio = (distancia / combustivel);
+        
+        console.log("O consumo médio do carro é de " + consumo_medio + " km por litro");
