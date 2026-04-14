0ed8fb83affbb8ad6b0c631162ae59f159045466
BARRAVELHA <matheusnplatt@gmail.com>
2026-04-14 17:37:35 -0300

    Add film duration calculations in filmes.js

---
+++ filmes.js
@@ -0,0 +1,9 @@
+let filme1 = 120;
+        let filme2 = 95;
+        let filme3 = 110;
+        
+        let tempo_total = (filme1 + filme2 + filme3);
+        let media_duracao = (tempo_total)/3;
+        
+        console.log("Tempo total assistido: " + tempo_total + " minutos");
+        console.log("Média de duração dos filmes: " + media_duracao + " minutos");
