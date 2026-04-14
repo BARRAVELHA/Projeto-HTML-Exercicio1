45aa1724e9a7fcc1208f6e590120c9e226fe97dc
BARRAVELHA <matheusnplatt@gmail.com>
2026-04-14 17:37:59 -0300

    Update jogo.js

---
+++ jogo.js
@@ -0,0 +1,11 @@
+let vidamaxima = 100;
+        let vida = vidamaxima;
+        let danoporespada = 30;
+        
+        vida -= danoporespada;
+        
+        if (vida > 0) {
+            console.log("Vida restante: " + vida + ". O personagem ainda está vivo!");
+        } else {
+            console.log("Vida restante: " + vida + ". O personagem foi derrotado!");
+        }
