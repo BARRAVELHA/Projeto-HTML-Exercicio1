dc487543691dff98aa314d41648ad05662a4d0cd
BARRAVELHA <matheusnplatt@gmail.com>
2026-04-14 18:35:50 -0300

    Refactor futebol.js to remove unnecessary lines

--- futebol.js
+++ futebol.js
@@ -4,8 +4,8 @@ let gols3 = 3;
 let gols4 = 0;
 let gols5 = 4;
         
-let total_gols = (gols1 + gols2 + gols3 + gols4 + gols5);
-let media_gols = (total_gols / 5);
+ let total_gols = (gols1 + gols2 + gols3 + gols4 + gols5);
+ let media_gols = (total_gols / 5);
         
-console.log("Total de gols marcados: " + total_gols);
-console.log("Média de gols por partida: " + media_gols);
+ console.log("Total de gols marcados: " + total_gols);
+ console.log("Média de gols por partida: " + media_gols);
