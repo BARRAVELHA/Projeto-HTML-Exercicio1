dcc29616ba5b440d271ace9fc94c715913749696
BARRAVELHA <matheusnplatt@gmail.com>
2026-04-14 17:42:15 -0300

    Update futebol.js

---
+++ futebol.js
@@ -0,0 +1,11 @@
+let gols1 = 2;
+let gols2 = 1;
+let gols3 = 3;
+let gols4 = 0;
+let gols5 = 4;
+        
+ let total_gols = (gols1 + gols2 + gols3 + gols4 + gols5);
+ let media_gols = (total_gols)/ 5;
+        
+ console.log("Total de gols marcados: " + total_gols);
+ console.log("Média de gols por partida: " + media_gols);
